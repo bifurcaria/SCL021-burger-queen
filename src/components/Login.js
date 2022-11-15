0ea6f4e7d3a5b4f6de599a7f9e2f8afe15453591
bifurcaria <c.carlajm@gmail.com>
2022-11-14 22:41:58 -0300

    add routes

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,22 @@ import { signGoogle } from '../lib/firebase.js';
 import 'semantic-ui-css/semantic.min.css';
 import { Button } from 'semantic-ui-react';
 import React from 'react';
+import { Container } from 'semantic-ui-react'
+
+//rendereado condicional, si estas logueado te muestra botones de waiter o cook
+//si no estas logueado sign in
+//Agegar sombrita
 
 const Login = () => {
+
     return (
+        <Container>
         <Button
         onClick={signGoogle}>
         entrar
         </Button>
+        </Container>
+        
     )
 }
 

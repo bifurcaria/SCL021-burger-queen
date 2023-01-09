6fa0d77d1dc9f19414ed23f3fbcf1444b4fa1949
bifurcaria <c.carlajm@gmail.com>
2023-01-09 20:04:30 -0300

    hash router

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import Hello from './Hello'
 import Cook from './Cook'
 import {
   createBrowserRouter,
-  RouterProvider,
+  HashRouter,
 } from "react-router-dom";
 import WaiterOrders from './WaiterOrders';
 
@@ -17,21 +17,21 @@ const router = createBrowserRouter([
     errorElement: <Hello />
   },
   {
-    path: "/SCL021-burger-queen/waiter",
+    path: "/waiter",
     element: <Waiter />,
   },
   {
-    path: "/SCL021-burger-queen/orders",
+    path: "/orders",
     element: <WaiterOrders />,
   },
   {
-    path: "/SCL021-burger-queen/cook",
+    path: "/cook",
     element: <Cook />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter router={router} />
   </React.StrictMode>
 );
